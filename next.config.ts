dda8d6e5547d8122963d2f67bc391cb8e7244e0d
Kroszborg <abhimanpanwar6@gmail.com>
2024-12-05 22:57:59 +0530

    fixes

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['placeholder.com'], // Add any other domains you might use for images
-  },
-}
-
-module.exports = nextConfig
-
